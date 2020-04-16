e93819b605c08b2ac8ecd0ba4c13e3ee560cfb82
Bayo Ade <bayo.300@gmail.com>
2020-04-16 11:38:46 +0100

    changed to link from BioText import

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 // Components
 import HeaderPic from '../components/headerPicture/headerPic'
-import BioText from '../components/BioText/biotext'
+import BioText from '../components/BioText/BioText'
 import Footer from '../components/Footer/Footer'
 
 const About = () => {
