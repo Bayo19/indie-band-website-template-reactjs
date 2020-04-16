18fe4bbf2e704eafc95e1511c8561f839419933c
Bayo Ade <bayo.300@gmail.com>
2020-04-16 11:15:41 +0100

    About jsx added

---
+++ src/pages/About.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+// Components
+import HeaderPic from '../components/headerPicture/headerPic'
+import BioText from '../components/BioText/biotext'
+import Footer from '../components/Footer/Footer'
+
+const About = () => {
+
+  return (
+    <div>
+      <div className="aboutContent">
+        <HeaderPic />
+        <BioText />
+        <Footer />
+      </div>
+    </div>
+  );
+}
+
+export default About
