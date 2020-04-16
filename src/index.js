709a01bd6387658c76bb84ea9c74563060755b63
Bayo Ade <bayo.300@gmail.com>
2020-04-16 10:36:49 +0100

    initial 3 index

--- src/index.js
+++ src/index.js
@@ -15,3 +15,36 @@ import News from './pages/News'
 import Shows from './pages/Shows'
 import About from './pages/About'
 import Contact from './pages/Contact'
+
+const App = () => {
+
+  let appAnime = useRef(null)
+
+  useEffect(function(){
+    TweenMax.from(appAnime, .99, {opacity: 0, ease: Power3.easeInOut})
+  })
+  return (
+    <>
+
+    <div ref={el => appAnime = el} className="App">
+    <Nav />
+    <div className="">
+      <Route exact={true} path='/' component={Home} />
+      <Route exact={true} path='/News' component={News} />
+      <Route exact={true} path='/Shows' component={Shows} />
+      <Route exact path='/About' component={About} />
+      <Route exact path='/Contact' component={Contact}/>
+    </div>
+    </div>
+    
+    </>
+  )
+}
+
+const rootElement = document.getElementById('root')
+hydrate(
+  <Router>
+    <App />
+  </Router>, 
+  rootElement
+)
