ac314da2914a853647cd271fc50a65645cddabd6
Bayo Ade <bayo.300@gmail.com>
2020-04-16 10:35:13 +0100

    initial 2 index

--- src/index.js
+++ src/index.js
@@ -2,3 +2,16 @@ import React, { useRef, useEffect } from 'react';
 import { hydrate } from 'react-dom';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { TweenMax, Power3 } from 'gsap'
+
+// Style
+import './index.css';
+
+// Components
+import Nav from './components/Nav/Nav'
+
+// Pages
+import Home from './pages/Home'
+import News from './pages/News'
+import Shows from './pages/Shows'
+import About from './pages/About'
+import Contact from './pages/Contact'
