5fb4058c6b5469122ae09cbb8a5fbd87695e057a
Bayo Ade <bayo.300@gmail.com>
2020-04-17 08:44:14 +0100

    added footer to news jsx page

--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -2,11 +2,14 @@ import React from "react";
 
 // Components
 import NewsLinks from '../components/News/newsLinks'
+import Footer from '../components/Footer/Footer'
+
 const News = () => {
     
   return (
     <div className="Home">
       <NewsLinks />
+      <Footer />
     </div>
   )
 
